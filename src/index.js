479a6eff047d17587f2b159ff75e80771646395f
shaokun <605416017@qq.com>
2024-07-02 23:42:00 +0800

    sync

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ files.sort();
 
 // sendTest("vmIOandFlowOperations/codecopy.json")
 
-for (let i = 5; i < files.length; i++) {
+for (let i = 7; i < files.length; i++) {
     await runTask({
         index: i,
         source: files[i],
