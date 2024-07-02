8049d578c914304f6fa0db8071ed0a7cfbdf426f
shaokun <605416017@qq.com>
2024-07-02 10:13:36 +0800

    sync

--- src/skip.js
+++ src/skip.js
@@ -14,9 +14,4 @@ export const IGNORE_TEST = [
         label: "all",
         comment: "No need to test performance, it depends on the move-vm implementation"
     },
-    // {
-    //     name: "ethereum-tests/GeneralStateTests/VMTests/vmTests/envInfo.json",
-    //     label: "all",
-    //     comment: "env info we think for our vm is not necessary"
-    // },
 ];
