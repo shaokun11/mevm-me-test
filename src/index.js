69e4150de24f301849154a4d406f85ef7effa5b9
shaokun <605416017@qq.com>
2024-07-08 13:50:57 +0800

    sync

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ async function listFiles() {
 const files = await listFiles();
 files.sort();
 
-for (let i = 119; i < files.length; i++) {
+for (let i = 120; i < files.length; i++) {
     await runTask({
         index: i,
         source: files[i],
