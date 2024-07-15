19766bae69f80a93f14419d7a5d417dd3e3cbda9
tonyxia2023 <tony@movemnt.dev>
2024-07-15 09:03:33 +0000

    sync

--- src/skip.js
+++ src/skip.js
@@ -94,4 +94,10 @@ export const IGNORE_TEST = [
         label: SKIP_ALL_LABEL,
         comment: MST_TIMEOUT,
     },
+    {
+        path: "ethereum-tests/GeneralStateTests/stStaticCall",
+        name: SKIP_ALL_NAME,
+        label: SKIP_ALL_LABEL,
+        comment: MST_TIMEOUT,
+    },
 ];
