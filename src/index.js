76d497aeeed9903ac2f13c9299b164a2db05be12
tonyxia2023 <tony@movemnt.dev>
2024-07-15 06:09:27 +0000

    sync

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ async function listFiles() {
 const files = await listFiles();
 files.sort();
 
-for (let i = 6; i < files.length; i++) {
+for (let i = 23; i < files.length; i++) {
     await runTask({
         index: i,
         source: files[i],
