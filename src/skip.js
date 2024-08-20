5cbf0859231c7451b955ec3825b6c5b79aaf7e30
tonyxia2023 <tony@movemnt.dev>
2024-08-20 07:57:53 +0000

    sync

--- src/skip.js
+++ src/skip.js
@@ -1,22 +1,23 @@
 export const SKIP_ALL_LABEL = "__all__";
 export const SKIP_ALL_NAME = "__all__";
 
-export const MSG_NOT_IMPLEMENTED_SELFDESTRUCT = "Not implementation SELFDESTRUCT opcode"
-const MSG_DROP_FROM_MOVE_VM = "Will execute long time and dropped from move-vm"
-const MSG_TO_DOUBLE_CHECK = "will execute error, need to double check"
-const MST_TIMEOUT="execute timeout"
-const MST_OUT_OF_GAS="out of gas"
-const MST_RLP="rlp decode error"
-export const MSG_NOT_SUPPORT_BLOB_TX = "Not support blob tx"
-export const MSG_NOT_SUPPORT_BLOB_BASEFEE = "Not support blob basefee opcode"
-export const MSG_NOT_SUPPORT_BLOB_HASH = "Not support blob hash opcode"
-export const MSG_NOT_SUPPORT_OPCODE_KZG = "Not support kzg opcode"
+export const MSG_NOT_IMPLEMENTED_SELFDESTRUCT = "Not implementation SELFDESTRUCT opcode";
+const MSG_DROP_FROM_MOVE_VM = "Will execute long time and dropped from move-vm";
+const MSG_TO_DOUBLE_CHECK = "will execute error, need to double check";
+const MSG_TIMEOUT = "execute timeout";
+const MSG_OUT_OF_GAS = "out of gas";
+const MSG_ONLY_URN_LOCAL = "only run local";
+const MSG_RLP = "rlp decode error";
+export const MSG_NOT_SUPPORT_BLOB_TX = "Not support blob tx";
+export const MSG_NOT_SUPPORT_BLOB_BASEFEE = "Not support blob basefee opcode";
+export const MSG_NOT_SUPPORT_BLOB_HASH = "Not support blob hash opcode";
+export const MSG_NOT_SUPPORT_OPCODE_KZG = "Not support kzg opcode";
 
-export const MOVE_VM_SKIP_BLOB_KEY = "0x1::evm_for_test: 0x4e2b"
-export const MOVE_VM_SKIP_BLOB_BASEFEE_KEY = "0x1::evm_for_test: 0x4a"
-export const MOVE_VM_SKIP_KZG_KEY = "0x1::evm_precompile: 0xa"
-export const MOVE_VM_SKIP_BLOB_HASH_KEY = "0x1::evm_for_test: 0x49"
-export const MOVE_VM_SKIP_SELFDESTRUCT_KEY = "0x1::evm_for_test: 0xff"
+export const MOVE_VM_SKIP_BLOB_KEY = "0x1::evm_for_test: 0x4e2b";
+export const MOVE_VM_SKIP_BLOB_BASEFEE_KEY = "0x1::evm_for_test: 0x4a";
+export const MOVE_VM_SKIP_KZG_KEY = "0x1::evm_precompile: 0xa";
+export const MOVE_VM_SKIP_BLOB_HASH_KEY = "0x1::evm_for_test: 0x49";
+export const MOVE_VM_SKIP_SELFDESTRUCT_KEY = "0x1::evm_for_test: 0xff";
 
 export const IGNORE_TEST = [
     {
@@ -29,19 +30,19 @@ export const IGNORE_TEST = [
         path: "ethereum-tests/GeneralStateTests/VMTests/vmPerformance",
         name: SKIP_ALL_NAME,
         label: SKIP_ALL_LABEL,
-        comment: MST_TIMEOUT,
+        comment: MSG_TIMEOUT,
     },
     {
         path: "ethereum-tests/GeneralStateTests/stAttackTest",
         name: SKIP_ALL_NAME,
         label: SKIP_ALL_LABEL,
-        comment: MST_TIMEOUT,
+        comment: MSG_TIMEOUT,
     },
     {
         path: "ethereum-tests/GeneralStateTests/Cancun/stEIP1153-transientStorage/15_tstoreCannotBeDosd.json",
         name: SKIP_ALL_NAME,
         label: SKIP_ALL_LABEL,
-        comment: MST_TIMEOUT,
+        comment: MSG_TIMEOUT,
     },
     {
         path: "ethereum-tests/GeneralStateTests/Cancun/stEIP4844-blobtransactions/createBlobhashTx.json",
@@ -53,37 +54,55 @@ export const IGNORE_TEST = [
         path: "ethereum-tests/GeneralStateTests/Cancun/stEIP1153-transientStorage/21_tstoreCannotBeDosdOOO.json",
         name: SKIP_ALL_NAME,
         label: SKIP_ALL_LABEL,
-        comment: MST_TIMEOUT,
+        comment: MSG_TIMEOUT,
     },
     {
         path: "ethereum-tests/GeneralStateTests/Pyspecs/cancun/eip1153_tstore/run_until_out_of_gas.json",
         name: SKIP_ALL_NAME,
         label: SKIP_ALL_LABEL,
-        comment: MST_TIMEOUT,
+        comment: MSG_TIMEOUT,
     },
     {
         path: "ethereum-tests/GeneralStateTests/stQuadraticComplexityTest/",
         name: SKIP_ALL_NAME,
         label: SKIP_ALL_LABEL,
-        comment: MST_TIMEOUT,
+        comment: MSG_TIMEOUT,
     },
     {
         path: "ethereum-tests/GeneralStateTests/stQuadraticComplexityTest/QuadraticComplexitySolidity_CallDataCopy.json",
         name: SKIP_ALL_NAME,
         label: SKIP_ALL_LABEL,
-        comment: MST_TIMEOUT,
+        comment: MSG_TIMEOUT,
     },
     {
         path: "ethereum-tests/GeneralStateTests/stStaticCall/static_Call1MB1024Calldepth.json",
         name: SKIP_ALL_NAME,
         label: SKIP_ALL_LABEL,
-        comment: MST_TIMEOUT,
+        comment: MSG_TIMEOUT,
     },
     {
         path: "ethereum-tests/GeneralStateTests/stStaticCall/static_Return50000_2.json",
         name: SKIP_ALL_NAME,
         label: SKIP_ALL_LABEL,
-        comment: MST_OUT_OF_GAS,
+        comment: MSG_OUT_OF_GAS,
+    },
+    {
+        path: "ethereum-tests/GeneralStateTests/stStaticCall/static_Call50000_identity.json",
+        name: SKIP_ALL_NAME,
+        label: SKIP_ALL_LABEL,
+        comment: MSG_ONLY_URN_LOCAL,
+    },
+    {
+        path: "ethereum-tests/GeneralStateTests/stStaticCall/static_Call50000_identity2.json",
+        name: SKIP_ALL_NAME,
+        label: SKIP_ALL_LABEL,
+        comment: MSG_TIMEOUT,
+    },
+    {
+        path: "ethereum-tests/GeneralStateTests/stStaticCall",
+        name: SKIP_ALL_NAME,
+        label: SKIP_ALL_LABEL,
+        comment: MSG_OUT_OF_GAS,
     },
     // {
     //     path: "ethereum-tests/GeneralStateTests/stCreate2/Create2Recursive.json",
